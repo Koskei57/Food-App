93bd0252e946b2360cb8972e3138b1518d31d9a4
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-07-27 01:24:22 +0300

    Style the component

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import style from "./css/Food.css";
 
 const Food = ({name,image,description}) => {
   return (
-      <div>
+      <div className="food">
           <img src={image} alt='Keroma loading' />
           <h2>{ name}</h2>
           <p>{description}</p>
