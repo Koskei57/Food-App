39d668e4a9af2d8e23729680f1481a6b2264873a
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-07-26 17:11:04 +0300

    Display food list

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const Food = () => {
+const Food = ({name,image,description}) => {
   return (
-    <div>Food</div>
+      <div>
+          <img src={image} alt='Keroma loading' />
+          <h2>{ name}</h2>
+          <p>{description}</p>
+      </div>
   )
 }
 
